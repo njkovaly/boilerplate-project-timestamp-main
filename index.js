bcd2c0f84c2674903cefa93689b5590bd62e5f37
njkovaly <kovaly@madisontelco.com>
2023-10-03 10:14:33 -0500

    changed

--- index.js
+++ index.js
@@ -70,8 +70,20 @@ app.get("/api/", (req, res) => {
 });
 
 // listen for requests :)
-var listener = app.listen(3000, function () {
+/*var listener = app.listen(3000, function () {
   console.log('Your app is listening on port 3000');
+}); */
+
+const http = require('http');
+
+const server = http.createServer(function(request, response) {
+    response.writeHead(200, { "Content-Type": "text/html" });
+    response.end("<html><body><h1>Hello World!</h1></body></html>");
 });
+
+const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log(`Server running at http://localhost:${port}`);
 
 
